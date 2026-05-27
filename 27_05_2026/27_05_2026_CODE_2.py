cfda39ae289838b5e16455ecf00019e67100792c
Shuvronil Ganguly <gangulyjaya0@gmail.com>
2026-05-27 23:50:43 +0530

    Solved LC 167, 179, 2165 in python

---
+++ 27_05_2026/27_05_2026_CODE_2.py
@@ -0,0 +1,27 @@
+#LC 179. Largest Number
+#CODE:
+
+from functools import cmp_to_key
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        nums = list(map(str, nums))
+
+        def compare(x, y):
+            if x + y > y + x:
+                return -1
+            elif x + y < y + x:
+                return 1
+            else:
+                return 0
+
+        nums.sort(key=cmp_to_key(compare))
+
+        result = ''.join(nums)
+
+        
+        if result[0] == '0':
+            return '0'
+
+        return result

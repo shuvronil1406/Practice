53d8bd3b00ba3af7cd3d6513ec06c12be90f2368
Shuvronil Ganguly <gangulyjaya0@gmail.com>
2026-06-20 22:40:43 +0530

    Solved LC 283, 560 in python

---
+++ 20_06_2026/20_06_2026_CODE_1.py
@@ -0,0 +1,14 @@
+#LC 560. Subarray Sum Equals K
+#CODE:
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        currSum = 0
+        prefixSums = {0: 1}
+        for i in nums:
+            currSum += i
+            diff = currSum - k
+            res  += prefixSums.get(diff,0)
+            prefixSums[currSum] = 1 + prefixSums.get(currSum,0)
+        return res
